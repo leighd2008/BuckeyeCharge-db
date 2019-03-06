f596b2166c475add390d148ef3322811504bf091
leighd2008 <leighd2008@gmail.com>
2019-03-06 15:26:56 -0500

    more spacing issues

--- src/TeamEvents/TeamEvents.js
+++ src/TeamEvents/TeamEvents.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './TeamEvents.css';
 import { Card, CardTitle } from 'reactstrap';
-import Scroll from '../Scroll';
+import Scroll from '../Scroll/Scroll';
 
 import Events from './Events'
 
@@ -9,7 +9,8 @@ const TeamEvents = ({eventUrls}) => {
     return ( 
       <Card className = "ma3 events"
         style = {
-            { backgroundColor: '#6c757d', borderColor: 'red', borderWidth: '4px', minWidth: '48vw' } } >
+            { backgroundColor: '#6c757d', borderColor: 'red', borderWidth: '4px',
+             minWidth: '48vw' } } >
         <CardTitle tag = "h1"> Team Events </CardTitle> 
         <Scroll>
           <Events eventUrls={eventUrls}/>
