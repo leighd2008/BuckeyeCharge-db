8036603b931b1d908d8cc5f269a3828d495ddd92
leighd2008 <leighd2008@gmail.com>
2019-03-19 14:19:45 -0400

    updated scoreboard for spring league

--- src/ScoreBoard/ScoreBoard.js
+++ src/ScoreBoard/ScoreBoard.js
@@ -9,8 +9,7 @@ const ScoreBoard = (props) => {
       <div className='lgscreen'>
         <Card className="scoreboard" >
           <CardTitle tag="h1">Scoreboard</CardTitle>
-          <iframe src="//gc.com/team-5bb0a41f0d6f575261bc197f/scoreboard?g=5&p=5bafd0a9b7ce48001b2f2705" width="728" height="90" margin="auto"
-              scrolling="no" frameBorder="0" title='scoreboard'></iframe>
+          <iframe src="//gc.com/team-5c85359b955e067a733c24e7/scoreboard?g=5&p=5bafd0a9b7ce48001b2f2705" width="728" height="90" scrolling="no" frameborder="0"></iframe>
         </Card>
       </div>
       {/* <div className='smscreen'>
