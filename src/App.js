c32a1891c1dd09cb37cdc9f5e4b210c05c490488
leighd2008 <leighd2008@gmail.com>
2019-04-02 16:15:19 -0400

    Added react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import './App.css';
 import Navigation from './Navigation/Navigation';
 import homeImage from './images/BCLogo-clr.png';
@@ -41,35 +42,52 @@ class App extends Component {
     }
 
   render() {
-    const project = () => {
-      switch(this.state.route) {
-        case "home": return <Home homeImage={this.state.homeImage}/>;
-        case "tournaments": return <TournamentPage />;
-        case "training": return <Training />;
-        case "tryouts": return <TryOuts homeImage={this.state.homeImage}/>;
-        case "Warren12U": return <TeamPage backgroundImage={this.state.backgroundImage} teamName={this.state.teamName}  eventUrls={this.state.eventUrls} />;
-        case "Cholley16U": return <TeamPage backgroundImage={this.state.backgroundImage} teamName={this.state.teamName}  eventUrls={this.state.eventUrls} />;
-        case "Cavanagh15U": return <TeamPage backgroundImage={this.state.backgroundImage} teamName={this.state.teamName} eventUrls={this.state.eventUrls} />;
-        case "Gardner13U": return <TeamPage backgroundImage={this.state.backgroundImage} teamName={this.state.teamName} eventUrls={this.state.eventUrls} />;
-        case "Yoder14U": return <TeamPage backgroundImage={this.state.backgroundImage} teamName={this.state.teamName} eventUrls={this.state.eventUrls} />;
-        case "Norman12U": return <TeamPage backgroundImage={this.state.backgroundImage} teamName={this.state.teamName} eventUrls={this.state.eventUrls} />;
-        case "Gressman11U": return <TeamPage backgroundImage={this.state.backgroundImage} teamName={this.state.teamName} eventUrls={this.state.eventUrls} />;
-        case "Albertson10U": return <TeamPage backgroundImage={this.state.backgroundImage} teamName={this.state.teamName} eventUrls={this.state.eventUrls} />;
-        case "Homan8U": return <TeamPage backgroundImage={this.state.backgroundImage} teamName={this.state.teamName} eventUrls={this.state.eventUrls} />;
-        case "Kaisk16U": return <TeamPage backgroundImage={this.state.backgroundImage} teamName={this.state.teamName} eventUrls={this.state.eventUrls} />;
-        case "abpitching": return <ABPitching />
-        default: return <h1>No team match</h1>
-      }
-    }
+    // const project = () => {
+      // switch(this.state.route) {
+        // case "home": return <Home homeImage={this.state.homeImage}/>;
+        // case "tournaments": return <TournamentPage />;
+        // case "training": return <Training />;
+        // case "tryouts": return <TryOuts homeImage={this.state.homeImage}/>;
+        // case "Warren12U": return <TeamPage backgroundImage={this.state.backgroundImage} teamName={this.state.teamName}  eventUrls={this.state.eventUrls} />;
+        // case "Cholley16U": return <TeamPage backgroundImage={this.state.backgroundImage} teamName={this.state.teamName}  eventUrls={this.state.eventUrls} />;
+        // case "Cavanagh15U": return <TeamPage backgroundImage={this.state.backgroundImage} teamName={this.state.teamName} eventUrls={this.state.eventUrls} />;
+        // case "Gardner13U": return <TeamPage backgroundImage={this.state.backgroundImage} teamName={this.state.teamName} eventUrls={this.state.eventUrls} />;
+        // case "Yoder14U": return <TeamPage backgroundImage={this.state.backgroundImage} teamName={this.state.teamName} eventUrls={this.state.eventUrls} />;
+        // case "Norman12U": return <TeamPage backgroundImage={this.state.backgroundImage} teamName={this.state.teamName} eventUrls={this.state.eventUrls} />;
+        // case "Gressman11U": return <TeamPage backgroundImage={this.state.backgroundImage} teamName={this.state.teamName} eventUrls={this.state.eventUrls} />;
+        // case "Albertson10U": return <TeamPage backgroundImage={this.state.backgroundImage} teamName={this.state.teamName} eventUrls={this.state.eventUrls} />;
+        // case "Homan8U": return <TeamPage backgroundImage={this.state.backgroundImage} teamName={this.state.teamName} eventUrls={this.state.eventUrls} />;
+        // case "Kaisk16U": return <TeamPage backgroundImage={this.state.backgroundImage} teamName={this.state.teamName} eventUrls={this.state.eventUrls} />;
+        // case "abpitching": return <ABPitching />
+        // default: return <h1>No team match</h1>
+      // }
+    // }
 
     return (
       <div className="App">
-        <Navigation
-          onRouteChange={this.onRouteChange}
-        />
-        <div>{ project() }</div>
-
+        <BrowserRouter>
+          <Navigation onRouteChange={this.onRouteChange} />
+          <Switch>
+            <Route exact path="/" component={() => <Home homeImage={this.state.homeImage} />} />
+            <Route exact path="/tournaments" component={() => <TournamentPage />} />
+            <Route exact path="/training" component={() => <Training />} />
+            <Route exact path="/tryouts" component={() => <TryOuts homeImage={this.state.homeImage}/>} />
+            <Route exact path="/Warren12U" component={() => <TeamPage backgroundImage={this.state.backgroundImage} teamName={this.state.teamName}  eventUrls={this.state.eventUrls} />} />
+            <Route exact path="/Cholley16U" component={() => <TeamPage backgroundImage={this.state.backgroundImage} teamName={this.state.teamName}  eventUrls={this.state.eventUrls} />} />
+            <Route exact path="/Cavanagh15U" component={() => <TeamPage backgroundImage={this.state.backgroundImage} teamName={this.state.teamName}  eventUrls={this.state.eventUrls} />} />
+            <Route exact path="/Gardner13U" component={() => <TeamPage backgroundImage={this.state.backgroundImage} teamName={this.state.teamName}  eventUrls={this.state.eventUrls} />} />
+            <Route exact path="/Yoder14U" component={() => <TeamPage backgroundImage={this.state.backgroundImage} teamName={this.state.teamName}  eventUrls={this.state.eventUrls} />} />
+            <Route exact path="/Norman12U" component={() => <TeamPage backgroundImage={this.state.backgroundImage} teamName={this.state.teamName}  eventUrls={this.state.eventUrls} />} />
+            <Route exact path="/Gressman11U" component={() => <TeamPage backgroundImage={this.state.backgroundImage} teamName={this.state.teamName}  eventUrls={this.state.eventUrls} />} />
+            <Route exact path="/Albertson10U" component={() => <TeamPage backgroundImage={this.state.backgroundImage} teamName={this.state.teamName}  eventUrls={this.state.eventUrls} />} />
+            <Route exact path="/Homan8U" component={() => <TeamPage backgroundImage={this.state.backgroundImage} teamName={this.state.teamName}  eventUrls={this.state.eventUrls} />} />
+            <Route exact path="/Kaisk16U" component={() => <TeamPage backgroundImage={this.state.backgroundImage} teamName={this.state.teamName}  eventUrls={this.state.eventUrls} />} />
+            <Route exact path="/abpitching" component={() => <ABPitching />} />
+          </Switch>
+        </BrowserRouter>
+          
       </div>
+
     );
   }
 }
--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 import BCLogo from '../images/BCLogo-blk-sml.png';
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 import './Navigation.css';
@@ -36,58 +37,58 @@ render() {
           <Collapse isOpen={this.state.isOpen} navbar >
             <Nav className="ml-auto" navbar >
               <NavItem >
-                <NavLink tag="h6" href="/" onClick={this.toggle}>
+                <NavLink tag={Link} to="/" onClick={this.toggle}>
                   <p onClick={() => onRouteChange('home')} >
-                    <br/> HOME 
+                    <br/> HOME
                   </p> 
                 </NavLink>
               </NavItem>
               <UncontrolledDropdown nav inNavbar >
                 <DropdownToggle tag="h6" nav caret ><br/>TEAMS </DropdownToggle>
                 <DropdownMenu >
-                  <DropdownItem onClick={this.toggle} >
+                  <DropdownItem className='teams' tag={Link} to="/Cholley16U" onClick={this.toggle} >
                     <p onClick={() => 
                       onRouteChange('Cholley16U', 'Cholley 16U', Ch16UURLS)} >Cholley 16U</p>
                   </DropdownItem>
-                  <DropdownItem onClick={this.toggle} >
+                  <DropdownItem tag={Link} to="/Kaisk16U" onClick={this.toggle} >
                     <p onClick={() => 
                       onRouteChange('Kaisk16U', 'Kaisk 16U', K16UURLS)} >Kaisk 16U</p>
                   </DropdownItem>
-                  <DropdownItem onClick={this.toggle} >
+                  <DropdownItem tag={Link} to="/Cavanagh15U"  onClick={this.toggle} >
                     <p onClick={() => 
                       onRouteChange('Cavanagh15U', 'Cavanagh 15U', C15UURLS)} >Cavanagh 15U</p>
                   </DropdownItem>
-                  <DropdownItem onClick={this.toggle} >
+                  <DropdownItem tag={Link} to="/Yoder14U" onClick={this.toggle} >
                     <p onClick={() => 
                       onRouteChange('Yoder14U', 'Yoder 14U', Y14UURLS)} >Yoder 14U</p>
                   </DropdownItem>
-                  <DropdownItem onClick={this.toggle} >
+                  <DropdownItem tag={Link} to="/Gardner13U" onClick={this.toggle} >
                     <p onClick={() => 
                       onRouteChange('Gardner13U', 'Gardner 13U', G13UURLS)} >Gardner 13U</p>
                   </DropdownItem>
-                  <DropdownItem onClick={this.toggle} >
+                  <DropdownItem tag={Link} to="/Norman12U" onClick={this.toggle} >
                     <p onClick={() => 
                       onRouteChange('Norman12U', 'Norman 12U', W12UURLS)} >Norman 12U</p>
                   </DropdownItem>
-                  <DropdownItem onClick={this.toggle} >
+                  <DropdownItem tag={Link} to="/Warren12U" onClick={this.toggle} >
                     <p onClick={() => 
                     onRouteChange('Warren12U', 'Warren 12U', N12UURLS)} >Warren 12U</p>
                   </DropdownItem>
-                  <DropdownItem onClick={this.toggle} >
+                  <DropdownItem tag={Link} to="/Gressman11U" onClick={this.toggle} >
                     <p onClick={() => 
                     onRouteChange('Gressman11U', 'Gressman 11U', G11UURLS)} >Gressman 11U</p>
                   </DropdownItem>
-                  <DropdownItem onClick={this.toggle} >
+                  <DropdownItem tag={Link} to="/Albertson10U" onClick={this.toggle} >
                     <p onClick={() => 
                       onRouteChange('Albertson10U', 'Albertson 10U', A10UURLS)} >Albertson10U</p>
                   </DropdownItem>
-                  <DropdownItem onClick={this.toggle} >
+                  <DropdownItem tag={Link} to="/Homan8U" onClick={this.toggle} >
                     <p onClick={() => 
                       onRouteChange('Homan8U', 'Homan 8U', H8UURLS)} >Homan8U</p>
                   </DropdownItem>
                 </DropdownMenu>
               </UncontrolledDropdown>
-              <NavLink tag="h6" href="/" onClick={this.toggle}>
+              <NavLink tag={Link} to="/tournaments" onClick={this.toggle}>
                 <p onClick={() => onRouteChange('tournaments')} >
                   <br/> TOURNAMENTS 
                 </p> 
@@ -97,22 +98,22 @@ render() {
                   <br/> FAN GEAR 
                 </a> 
               </NavLink>
-              {/* <NavLink tag="h6" href="/" onClick={this.toggle}>
+              {/* <NavLink tag="h6" to="/" onClick={this.toggle}>
                 <p onClick={() => onRouteChange('home')} >
                   <br/> SPONSORS 
                 </p> 
               </NavLink> */}
-              <NavLink tag="h6" href="/" onClick={this.toggle}>
+              <NavLink tag={Link} to="/training" onClick={this.toggle}>
                 <p onClick={() => onRouteChange('training')} >
                   <br/> TRAINING 
                 </p> 
               </NavLink>
-              <NavLink tag="h6" href="/" onClick={this.toggle}>
+              <NavLink tag={Link} to="/tryouts" onClick={this.toggle}>
                 <p onClick={() => onRouteChange('tryouts')} >
                   <br/> TRY-OUTS 
                 </p> 
               </NavLink>
-              <NavLink tag="h6" href="/" onClick={this.toggle}>
+              <NavLink tag={Link} to="/abpitching" onClick={this.toggle}>
                 <p onClick={() => onRouteChange('abpitching')} >
                   AB Pitching,<br/> Hitting <br/>and Fitness 
                 </p> 
